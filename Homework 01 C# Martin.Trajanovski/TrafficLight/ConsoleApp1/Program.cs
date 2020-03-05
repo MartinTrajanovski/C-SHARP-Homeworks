a8fe92af61425f23e0d80844cfc807d60e04fd7e
MartinTrajanovski <55089636+MartinTrajanovski@users.noreply.github.com>
2020-03-05 19:44:39 +0100

    Add files via upload

---
+++ Homework 01 C# Martin.Trajanovski/TrafficLight/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor
+            = ConsoleColor.Red;
+            Console.WriteLine("STOP",
+                           Console.BackgroundColor);
+            Thread.Sleep(2000);
+            Console.BackgroundColor
+            = ConsoleColor.Yellow;
+            Console.WriteLine("READY",
+                           Console.BackgroundColor);
+            Thread.Sleep(2000);
+            Console.ResetColor();
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.BackgroundColor
+            = ConsoleColor.DarkGreen;
+            Console.WriteLine("GO",
+                           Console.BackgroundColor);
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
